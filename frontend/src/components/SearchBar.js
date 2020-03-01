6f46519b59ac5bab279e585beaeb1a1cc271ba09
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-03-01 16:10:00 -0300

    componente searchbar

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function SearchBar() {
+    const [term, setTerm] = useState('')
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="pesquisar" onChange={e => setTerm(e.target.value)} value={term}></input>
+                <button>search</button>
+            </form>
+        </div>
+    )
+}
